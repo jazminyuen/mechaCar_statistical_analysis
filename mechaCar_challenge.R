134e78a90ea07227e1ef8e5230da334bbf1c16a2
Jazmin Capuli Yuen <jaz@jazmins-air.lan>
2022-05-21 14:17:24 -0700

    Suspension coil PSI stats

--- mechaCar_challenge.R
+++ mechaCar_challenge.R
@@ -10,3 +10,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Determine p-value and r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table))
 
+# Import and read in csv as a dataframe
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Summarize the suspension coil's PSI column
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),
+                                                Median=median(PSI),
+                                                Variance=var(PSI),
+                                                SD=sd(PSI))
+# Summarize the PSI metrics for each lot
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                              Median=median(PSI),
+                                                                              Variance=var(PSI),
+                                                                              SD=sd(PSI))
