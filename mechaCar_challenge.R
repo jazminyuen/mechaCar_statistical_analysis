46eaaeb024c1c1fc4c482df443e3345a5d9ff6fc
Jazmin Capuli Yuen <jaz@jazmins-air.lan>
2022-05-21 15:52:32 -0700

    T-tests on suspension coils

--- mechaCar_challenge.R
+++ mechaCar_challenge.R
@@ -10,6 +10,8 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Determine p-value and r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table))
 
+
+
 # Import and read in csv as a dataframe
 suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
@@ -23,3 +25,22 @@ lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Me
                                                                               Median=median(PSI),
                                                                               Variance=var(PSI),
                                                                               SD=sd(PSI))
+
+
+
+# Use t-Test to determine if PSI across all manufacturing lots is statistically different from the population mean of 1500 PSI
+t.test(suspension_table$PSI,mu=1500)
+
+# Create subsets for the lots
+lot_one <- subset(suspension_table, Manufacturing_Lot=="Lot1")
+lot_two <- subset(suspension_table, Manufacturing_Lot=="Lot2")
+lot_three <- subset(suspension_table, Manufacturing_Lot=="Lot3")
+
+# Test Manufacturing Lot 1
+t.test(lot_one$PSI,mu=1500)
+
+# Test Manufacturing Lot 2
+t.test(lot_two$PSI,mu=1500)
+
+# Test Manufacturing Lot 2
+t.test(lot_three$PSI,mu=1500)
